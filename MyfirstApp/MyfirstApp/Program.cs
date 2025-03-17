ae4ef7bda1b96200a872c729c35225a3ec1e4843
Kiruthika <cemcakiruthika23@gmail.com>
2025-03-17 22:11:37 +0530

    entire code of custom middleware

---
+++ MyfirstApp/MyfirstApp/Program.cs
@@ -0,0 +1,22 @@
+using MyfirstApp.customMiddleware;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddTransient<mycustom>();
+var app = builder.Build();
+//middleware1
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    await context.Response.WriteAsync("Hello");
+    await next(context);
+});
+//middleare 2
+app.UseMiddleware<mycustom>();
+
+//middleware 3
+app.Run(async (HttpContext context) =>
+{
+    await context.Response.WriteAsync("kiruthi");
+});
+
+
+app.Run();
---
+++ MyfirstApp/MyfirstApp/customMiddleware/mycustom.cs
@@ -0,0 +1,12 @@
+﻿namespace MyfirstApp.customMiddleware
+{
+    public class mycustom : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            await context.Response.WriteAsync("my custome middle ware start");
+            await next(context);
+            await context.Response.WriteAsync("my custome middleware last");
+        }
+    }
+}
